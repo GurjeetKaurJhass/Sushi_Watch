533f43c0291d987398474b920091f10d94ed352e
Gurjit Babrah <gurjit.babrah@gmail.com>
2019-11-01 17:31:32 -0400

    added game sushi tower logic to  phone

---
+++ Sushi_watch/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Sushi_watch
+//
+//  Created by Gurjeet kaur on 2019-11-01.
+//  Copyright © 2019 The Lambton. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size:self.view.bounds.size)
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        
+        // property to show hitboxes
+        skView.showsPhysics = true
+        
+        skView.presentScene(scene)
+        
+    }
+    
+    
+}
